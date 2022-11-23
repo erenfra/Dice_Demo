d5755c69dc4315171eebda6f483ab4902cfb54d0
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-11-23 09:53:58 -0600

    Initial Commit

---
+++ Dice_Demo/Dice_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_DemoApp.swift
+//  Dice_Demo
+//
+//  Created by Renato Oliveira Fraga on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
